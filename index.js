865ec089ca0c8fffd5b6c20d8cbb47a719c55d39
Francesco Sirocco <sirocco.francesco@gmail.com>
2016-07-18 13:48:08 +0200

    minor

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-var debug = require('debug')('jSaturday::require');
+var debug = require('debug')('jsaturday::require');
 
-module.exports = function(settings){
+module.exports = function(folderArray){
 
-  var customFolder = settings.customFolder || 'Custom';
-  var baseDir = settings.baseDir || __dirname;
+  if(!folderArray)
+    folderArray = [];
+
+  // Normalize string to arrray
+  if(!Array.isArray(folderArray) && typeof folderArray === 'string')
+    folderArray = [folderArray];
+
+  if(!Array.isArray(folderArray)){
+    throw new Error('Argument must be string or array');
+  }
 
   return function(moduleName){
     var module;
-    try{
-      module = require(baseDir + '/' + customFolder + '/' + moduleName);
-      return module;
-    }catch(err){
-      debug('info', 'Not found custom ' + baseDir + '/' + customFolder + '/' + moduleName);
-    }
 
-    debug('info', 'Searching in default path for ' + moduleName);
+    for(var i = 0; i < folderArray.length; i++){
+      var folder = folderArray[i];
+
+      try{
+        module = require(folder + '/' + moduleName);
+        return module;
+      }catch(err){
+        debug('debug', 'Not found ' + moduleName + ' in ' + folder);
+      }
+    }
 
     module = require(moduleName);
     return moduleName;
-  }
+  };
 
-}
+};
--- test/test.js
+++ test/test.js
@@ -1,18 +1,26 @@
-var jSatRequire = require('../index')({
-	baseDir: __dirname,
-	customFolder: 'Custom_Folder'
-});
 var mocha = require('mocha');
 var should = require('should');
 
 describe('# jSaturday_require', function(){
 
-	it('# Should return original module if not in Custom', function(done){
-		var debug = jSatRequire('debug')
+	it('# Should return error if was not passed array or string', function(done){
+		try{
+			var jSatRequire = require('../index')({});
+		}catch(err){
+			err.message.should.equal('Argument must be string or array');
+			done();	
+		}	
+	});	
+
+	it('# Should return original module if not in custom folder', function(done){
+		var jSatRequire = require('../index')(__dirname + '/Custom_Folder');
+		var debug = jSatRequire('debug');
 		done();
 	});
+
 	it('# Should overwrite module if present in Custom', function(done){
-		var test = jSatRequire('env-settings')
+		var jSatRequire = require('../index')(__dirname + '/Custom_Folder');
+		var test = jSatRequire('env-settings');
 		test.should.equal('IT IS WORKING');
 		done();
 	});
